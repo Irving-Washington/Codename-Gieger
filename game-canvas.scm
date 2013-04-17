c6a07dc4eed955dd2b691eb33eb9bfca238c3a7a
Lage Ragnarsson <lagge92@gmail.com>
2013-04-17 20:50:31 +0300

    re-commiting lost data
    game-canvas% now communicates with interaction.

--- game-canvas.scm
+++ game-canvas.scm
@@ -1,21 +1,26 @@
 (define game-canvas%
+ 
   (class canvas%
+    
     (inherit get-width get-height refresh)
-
+    (init-field *interaction*)
     (define aux-eventspace (make-eventspace))
 
-    (define/override (on-char ke)
+    (define/override (on-char key-event)
       (parameterize ((current-eventspace aux-eventspace))
         (queue-callback
          (lambda ()
-           (display (send ke get-key-code))
+           (send *interaction* new-key-event (send key-event get-key-code))
+           (send *interaction* new-key-event (send key-event get-key-release-code))
            (refresh)))))
     
-    (define/override (on-event me)
+    (define/override (on-event mouse-event)
       (parameterize ((current-eventspace aux-eventspace))
         (queue-callback
          (lambda ()
-           (display (send me get-event-type))
+           (send *interaction* new-mouse-event (send mouse-event get-mouse-event))
+           (send *interaction* new-mouse-position (cons (send mouse-event get-x)
+                                                        (send mouse-event get-y)))
            (refresh)))))
 
     (super-new)))
