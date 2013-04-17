2c331f385871ed3b87a534273babb2cb652e3583
Lage Ragnarsson <lagge92@gmail.com>
2013-04-17 20:43:26 +0300

    re-commiting lost data
    added method set-aim-target!

--- agent.scm
+++ agent.scm
@@ -6,9 +6,10 @@
     
     (field (health 100))
     (field (radiation 0))
-    (field (velocity (mcons 0 0))
+    (field (velocity (mcons 0 0)))
     (field (aim-direction (/ pi 2)))
-    (field (inventory (cons #f #f)))
+    (field (inventory (mcons #f #f)))
+    (field (max-velocity 5))
     
     (define/public (get-team) team)
     
@@ -24,39 +25,59 @@
     
     ;Velocity methods
     (define/public (get-velocity) velocity)
-    (define/public (set-velocity-x! delta-value)
+    (define/public (set-velocity-x! value)
+      (set-mcar! velocity value))
+    (define/public (set-velocity-y! value)
+      (set-mcdr! velocity value))
+    (define/public (increase-velocity-x! delta-value)
       (set-mcar! velocity (+ (mcar velocity) delta-value)))
-    (define/public (set-velocity-y! delta-value)
+    (define/public (increase-velocity-y! delta-value)
       (set-mcdr! velocity (+ (mcdr velocity) delta-value)))
     
     ;Aim-direction methods
     (define/public (get-aim-direction) aim-direction)
     (define/public (set-aim-direction! angle)
       (set! aim-direction angle))
-   
-    ;Inventory methods
-    (define/public (item-add! item)
-      (cond ((not (mcar inventory)) (set-mcar! inventory item))
-            ((not (mcdr inventory)) (set-mcdr! inventory item))
-            (else (set-mcar! inventory item))))
-    
-    (define/public (item-use)
-      (send (mcar hand-items) use))
-    
-    (define/public (item-remove-primary!)
-      (set-mcar! inventory #f))
-    
-    (define/public (item-remove-secondary!)
-      (set-mcdr! inventory #f))
-    
-    ;Move method
-    (define/public (move!)
-      (set-mcar! position (+ (mcar position) (mcar velocity)))
-      (set-mcdr! position (+ (mcdr position) (mcdr velocity))))
-    
-    ;Die method
-    (define/public (die)
-      (void))
-   
-    (super-new))))
+    (define/public (set-aim-target! coordinates)
+      (let ((x-position (+ 16 (car position)))
+            (y-position (+ 16 (cdr position)))
+            (x-coordinates (car coordinates))
+            (y-coordinates (cdr coordinates)))
+        (cond ((or (and (> (- x-coordinates x-position) 0)
+                        (> (- y-coordinates y-position) 0))
+                   (and (> (- x-coordinates x-position) 0)
+                        (< (- y-coordinates y-position) 0)))
+               (set-aim-direction! (atan (/ (- y-position y-coordinates)
+                                            (- x-position x-coordinates)))))
+              ((or (and (< (- x-coordinates x-position) 0)
+                        (> (- y-coordinates y-position) 0))
+                   (and (< (- x-coordinates x-position) 0)
+                        (< (- y-coordinates y-position) 0)))
+               (set-aim-direction! (+ 3.1416 (atan (/ (- y-position y-coordinates)
+                                                      (- x-position x-coordinates)))))))))
       
+      ;Inventory methods
+      (define/public (item-add! item)
+        (cond ((not (mcar inventory)) (set-mcar! inventory item))
+              ((not (mcdr inventory)) (set-mcdr! inventory item))
+              (else (set-mcar! inventory item))))
+      
+      (define/public (item-use)
+        (send (mcar hand-items) use))
+      
+      (define/public (item-remove-primary!)
+        (set-mcar! inventory #f))
+      
+      (define/public (item-remove-secondary!)
+        (set-mcdr! inventory #f))
+      
+      ;Move method
+      (define/public (move!)
+        (set-mcar! position (+ (mcar position) (mcar velocity)))
+        (set-mcdr! position (+ (mcdr position) (mcdr velocity))))
+      
+      ;Die method
+      (define/public (die)
+        (void))
+      
+      (super-new)))
